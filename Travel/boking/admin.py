ccb0d76a712ba3e335f9ea135e3ddea592a70a8f
a22616 <81513107+a22616@users.noreply.github.com>
2021-07-17 16:18:22 +0530

    Add files via upload

---
+++ Travel/boking/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from boking.models import FLIGHT,HOTEL,CONTACT
+# Register your models here.
+admin.site.register(FLIGHT)
+admin.site.register(HOTEL)
+admin.site.register(CONTACT)
+
---
+++ Travel/boking/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BokingConfig(AppConfig):
+    name = 'boking'
